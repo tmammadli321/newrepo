4e1d5adb80fbec538e4b422a954a2d2f99624a8f
jumba-salsa <mertvienebesa0@mail.ru>
2023-09-15 20:47:31 +0000

    cosmetic

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import mysql.connector
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_data():
     conn = mysql.connector.connect(
         host='localhost',
@@ -11,11 +11,11 @@ def get_data():
         password='2304',
         database='mydb'
     )
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)  # Use dictionary=True to get column names
     cursor.execute('SELECT * FROM my_table')
     data = cursor.fetchall()
     conn.close()
-    return jsonify(data)
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- initdb/init.sql
+++ initdb/init.sql
@@ -1,8 +1,16 @@
-CREATE DATABASE mydb;
+CREATE DATABASE IF NOT EXISTS mydb;
 USE mydb;
 
+DROP TABLE IF EXISTS my_table;
 CREATE TABLE my_table(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT
 );
+
+INSERT INTO my_table(name, description) VALUES
+    ('John', 'John is a common English name and is derived from the Hebrew name Yohanan.'),
+    ('Alice', 'Alice is of Old German origin, meaning "noble". It is often associated with fairy tales and adventures.'),
+    ('Bob', 'Bob is a diminutive of Robert. Robert is of Old German origin, meaning "bright fame".'),
+    ('Eve', 'Eve is derived from the Hebrew name Hawwa. In Christian belief, Eve was the first woman on Earth.'),
+    ('Charlie', 'Charlie is a diminutive form of Charles. Charles has a Germanic origin, meaning "free man".');
